9ada37fe2a10bbbbff7da3f370a664eb1b4634e8
Alexander Zhan <96319134+azhan3@users.noreply.github.com>
2023-04-24 00:29:12 -0400

    Delete Quiz directory

--- Quiz/MultipleChoice.java
+++
@@ -1,6 +0,0 @@
-package Quiz;
-public class MultipleChoice extends Question {
-    public MultipleChoice(String question, String choiceA, String choiceB, String choiceC, String choiceD, String correctAnswer) {
-        super(question, new String[]{choiceA, choiceB, choiceC, choiceD}, correctAnswer);
-    }
-}
